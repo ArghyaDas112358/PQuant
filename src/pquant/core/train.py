53039154fdac245d027e0627a0def4a45e3b8919
Arghya112358 <arghyaranjandas@gmail.com>
2025-06-27 21:13:33 +0200

    nicer training loop

--- src/pquant/core/train.py
+++ src/pquant/core/train.py
@@ -4,7 +4,7 @@ import keras
 def iterative_train(model, config, train_func, valid_func, **kwargs):
     if keras.backend.backend() == "torch":
         from pquant.core.torch_impl.train_torch import iterative_train_torch
-
+        print("yayy")
         return iterative_train_torch(model, config, train_func, valid_func, **kwargs)
     else:
         from pquant.core.tf_impl.train_tf import iterative_train_tf
