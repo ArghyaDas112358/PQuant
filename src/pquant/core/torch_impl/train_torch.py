38a127f9cc0720f0fe454df1c864e55fb1037168
Arghya112358 <arghyaranjandas@gmail.com>
2025-06-27 20:28:02 +0200

    nicer training loop

--- src/pquant/core/torch_impl/train_torch.py
+++ src/pquant/core/torch_impl/train_torch.py
@@ -1,4 +1,6 @@
 import torch
+from tqdm.auto import tqdm  
+import collections
 
 from pquant.core.torch_impl.compressed_layers_torch import (
     call_post_round_functions,
@@ -9,6 +11,14 @@ from pquant.core.torch_impl.compressed_layers_torch import (
     save_weights_functions,
 )
 
+class History:
+    def __init__(self):
+        self.history = collections.defaultdict(list)
+    
+    def on_epoch_end(self, epoch, logs=None):
+        logs = logs or {}
+        for k, v in logs.items():
+            self.history[k].append(v)
 
 def iterative_train_torch(model, config, train_func, valid_func, **kwargs):
     """
@@ -16,8 +26,12 @@ def iterative_train_torch(model, config, train_func, valid_func, **kwargs):
     """
     epoch = torch.tensor(0)  # Keeps track of all the epochs completed
     training_config = config["training_parameters"]
+
+    # Pre-training loop 
     if training_config["pretraining_epochs"] > 0:
-        for e in range(training_config["pretraining_epochs"]):
+        pretrain_epochs = range(training_config["pretraining_epochs"])
+        pbar = tqdm(pretrain_epochs, desc="Pre-training")
+        for e in pbar:
             model.train()
             pre_epoch_functions(model, e, training_config["pretraining_epochs"])
             train_func(model, epoch=epoch, **kwargs)
@@ -25,9 +39,18 @@ def iterative_train_torch(model, config, train_func, valid_func, **kwargs):
             valid_func(model, epoch=epoch, **kwargs)
             post_epoch_functions(model, e, training_config["pretraining_epochs"])
             epoch += 1
-    post_pretrain_functions(model, config)
-    for r in range(training_config["rounds"]):
-        for e in range(training_config["epochs"]):
+        post_pretrain_functions(model, config)
+
+    # Main training loop 
+    rounds = range(training_config["rounds"])
+    outer_pbar = tqdm(rounds, desc="Training Rounds")
+    for r in outer_pbar:
+        
+        outer_pbar.set_description(f"Round {r + 1}/{training_config['rounds']}")
+        inner_epochs = range(training_config["epochs"])
+        inner_pbar = tqdm(inner_epochs, desc="Epochs", leave=False)
+        
+        for e in inner_pbar:
             model.train()
             if r == 0 and training_config["save_weights_epoch"] == e:
                 save_weights_functions(model)
@@ -38,9 +61,13 @@ def iterative_train_torch(model, config, train_func, valid_func, **kwargs):
             post_epoch_functions(model, e, training_config["epochs"])
             epoch += 1
         call_post_round_functions(model, training_config["rewind"], training_config["rounds"], r)
+
+    # Fine-tuning loop
     pre_finetune_functions(model)
     if training_config["fine_tuning_epochs"] > 0:
-        for e in range(training_config["fine_tuning_epochs"]):
+        finetune_epochs = range(training_config["fine_tuning_epochs"])
+        pbar = tqdm(finetune_epochs, desc="Fine-tuning")
+        for e in pbar:
             model.train()
             pre_epoch_functions(model, e, training_config["fine_tuning_epochs"])
             train_func(model, epoch=epoch, **kwargs)
@@ -48,4 +75,5 @@ def iterative_train_torch(model, config, train_func, valid_func, **kwargs):
             valid_func(model, epoch=epoch, **kwargs)
             post_epoch_functions(model, e, training_config["fine_tuning_epochs"])
             epoch += 1
+            
     return model
